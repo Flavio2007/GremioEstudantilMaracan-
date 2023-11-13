95feb444f91e341d73d52637b464036bebd9ae11
Flavio2007 <129765711+Flavio2007@users.noreply.github.com>
2023-11-13 11:40:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocarSomAplausos(){
+document.querySelector('#som_tecla_aplausos').play();
+}
+
+document.querySelector('.tecla_aplausos').onclick = function(){
+    document.quertySelector('tecla_pom').onclick = tocaSomPom;
+}
