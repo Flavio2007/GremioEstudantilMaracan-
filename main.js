1a4320c7bedd51b23b62f25a51e9ce72dfa39f58
Flavio2007 <129765711+Flavio2007@users.noreply.github.com>
2023-11-27 11:48:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 function tocarSomAplausos(){
 document.querySelector('#som_tecla_aplausos').play();
 }
+document.quertySelector('.tecla_aplausos').onclick = tocaSomAplausos;
 
-document.querySelector('.tecla_aplausos').onclick = function(){
-    document.quertySelector('tecla_pom').onclick = tocaSomPom;
-}
+function tocarSomVaia(){
+    document.querySelector('#som_tecla_vaia').play();
+    }
+    document.quertySelector('.tecla_vaia').onclick = tocaSomVaia;
+
+function tocarSomRisada(){
+      document.querySelector('#som_tecla_risada').play();
+        }
+     document.quertySelector('.tecla_risada').onclick = tocaSomRisada;
+
+function tocarSomErrado(){
+        document.querySelector('#som_tecla_errado').play();
+            }
+        document.quertySelector('.tecla_errado').onclick = tocaSomErrado;
+
+ function tocarSomGrilo(){
+        document.querySelector('#som_tecla_grilo').play();
+            }
+        document.quertySelector('.tecla_grilo').onclick = tocaSomGrilo;
+
+function tocarSomTambores(){
+        document.querySelector('#som_tecla_tambores').play();
+            }
+        document.quertySelector('.tecla_tambores').onclick = tocaSomTambores;
+
+function tocarSomTrombeta(){
+        document.querySelector('#som_tecla_trombeta').play();
+                }
+        document.quertySelector('.tecla_trombeta').onclick = tocaSomTrombeta;
+
+function tocarSomDinheiro(){
+        document.querySelector('#som_tecla_dinheiro').play();
+                }
+        document.quertySelector('.tecla_dinheiro').onclick = tocaSomDinheiro;
+
+function tocarSomVitoria(){
+        document.querySelector('#som_tecla_vitoria').play();
+                }
+        document.quertySelector('.tecla_vitoria').onclick = tocaSomVitoria;
